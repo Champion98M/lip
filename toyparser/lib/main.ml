21e268acd1a2974faacbefbd52abe73c57e7a235
Alessio Mugnaini <aragornthekids@gmail.com>
2023-12-06 01:26:50 +0100

    toyparser done

--- toyparser/lib/main.ml
+++ toyparser/lib/main.ml
@@ -7,14 +7,38 @@ let parse (s : string) : ast =
   let ast = Parser.prog Lexer.read_token lexbuf in
   ast
 
-type result = int
+type result = int option
 
-let string_of_result n = string_of_int n
+let string_of_result = function
+  | Some n -> string_of_int n
+  | None -> "Error"
     
 (* eval : ast -> result *)
     
 let rec eval = function
-    Const(n) -> n
-  | Add(e1,e2) -> eval e1 + eval e2
+    Const(n) ->  Some n
+  | Add(e1,e2) -> 
+      (match (eval e1, eval e2) with
+        | (Some v1, Some v2) -> Some (v1+v2)
+        | _ -> None)
+  | Sub(e1,e2) -> 
+      (match (eval e1, eval e2) with
+        | (Some v1, Some v2) -> Some (v1-v2)
+        | _ -> None)
+  | Mul(e1,e2) -> 
+      (match (eval e1, eval e2) with
+        | (Some v1, Some v2) -> Some (v1*v2)
+        | _ -> None)
+  | Div(e1,e2) ->
+      (match (eval e1, eval e2) with
+        | (_, Some 0) -> None
+        | (Some v1, Some v2) -> Some (v1/v2)
+        | _ -> None)
+  | Minus(e) ->
+      (match eval e with
+        | Some e1 -> Some (-e1)
+        | _ -> None)
+  | Hex(n) -> Some n
+
 
                     
--- toyparser/lib/parser.mly
+++ toyparser/lib/parser.mly
@@ -7,8 +7,15 @@ open Ast
 %token LPAREN
 %token RPAREN
 %token EOF
+%token SUB
+%token MUL
+%token DIV
+%token <string> HEX
 
 %left PLUS
+%left SUB
+%left MUL
+%left DIV
 
 %start <ast> prog
 
@@ -21,5 +28,10 @@ prog:
 expr:
   | n = CONST { Const(int_of_string n) }
   | e1 = expr; PLUS; e2 = expr { Add(e1,e2) }
+  | e1 = expr; SUB; e2 = expr { Sub(e1,e2) }
+  | e1 = expr; MUL; e2 = expr { Mul(e1,e2) }
+  | e1 = expr; DIV; e2 = expr { Div(e1,e2) }
+  | SUB; e1 = expr { Minus(e1) }
+  | n = HEX { Hex(int_of_string n) }
   | LPAREN; e=expr; RPAREN {e}
 ;
